7b1986b79382ccd72796d679cbeea0eae29e1727
Ethan Moore <47327967+EtanDev@users.noreply.github.com>
2019-02-06 21:21:05 -0600

    Update MyScriptLoops.cs

--- MyScriptLoops.cs
+++ MyScriptLoops.cs
@@ -2,27 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyScriptLoops : MonoBehaviour {
+public class MyScriptLoops : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+        	for (int i = 0; i < 10; i++)
+        		{
+            		Debug.Log("Printed " + i);
+        		}
 
-        for (int i = 0; i < 10; i++)
-        {
-            Debug.Log("Printed " + i);
-        }
+        	int a = 0;
 
-        int a = 0;
-
-        while(true)
-        {
-            Debug.Log("printed " + a);
-            a++;
-        }
+        	while(true)
+        		{
+            		Debug.Log("printed " + a);
+            		a++;
+        		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
