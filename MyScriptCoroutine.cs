3d3b331a19e55e4882e73403c1fc99933e9c6c0d
Ethan Moore <47327967+EtanDev@users.noreply.github.com>
2019-02-06 21:19:21 -0600

    Update MyScriptCoroutine.cs

--- MyScriptCoroutine.cs
+++ MyScriptCoroutine.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyScriptCoroutine : MonoBehaviour {
+public class MyScriptCoroutine : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
         StartCoroutine(executeSomething());
-       /* 
+       	/* 
         StartCoroutine("executeSomething");
         StopCoroutine("executeSomething");
-	    */
+	*/
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 
