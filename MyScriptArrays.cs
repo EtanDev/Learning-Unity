b75c373e1ed8b4d42cc11a848ce0afb19b7d4d9c
Ethan Moore <47327967+EtanDev@users.noreply.github.com>
2019-02-06 21:18:12 -0600

    Update MyScriptArrays.cs

--- MyScriptArrays.cs
+++ MyScriptArrays.cs
@@ -2,26 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyScriptArrays : MonoBehaviour {
+public class MyScriptArrays : MonoBehaviour 
+{
 
     int[] nums = new int[10];
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+        	for(int i = 0; i < nums.Length; i++)
+        		{
+            		nums[i] = Random.Range(1, 100);
+        		}
 
-        for(int i = 0; i < nums.Length; i++)
-        {
-            nums[i] = Random.Range(1, 100);
-        }
-
-        for (int i = 0; i < nums.Length; i++)
-        {
-            Debug.Log("Element: " + i + " is " + nums[i]);
-        }
+        	for (int i = 0; i < nums.Length; i++)
+        		{
+            		Debug.Log("Element: " + i + " is " + nums[i]);
+        		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
