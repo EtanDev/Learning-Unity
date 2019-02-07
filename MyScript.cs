3e51952e3a42b7883ac6d44418fe1eda2cde85d9
Ethan Moore <47327967+EtanDev@users.noreply.github.com>
2019-02-06 21:16:24 -0600

    Update MyScript.cs

--- MyScript.cs
+++ MyScript.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyScript : MonoBehaviour {
-
+public class MyScript : MonoBehaviour 
+{
    
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
         Debug.Log("Printed from script");
-        
-	}
+        }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
